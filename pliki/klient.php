c132f130971ec86d63984f1818a1dd2b1cd4ee14
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2021-10-20 16:37:01 +0200

    Tabela z danymi
    Tabela z danymi klientów i style do niej

--- pliki/klient.php
+++ pliki/klient.php
@@ -1,1 +1,17 @@
-<a href="index.php?modul=klient_formularz">Dodaj klienta</a>
+<?php
+$tabela = "<table><tr><th>Id</th><th>Imie</th><th>Nazwisko</th><th>Telefon</th><th>Adres</th></tr>";
+
+$query = "SELECT * FROM `klient`";
+$rezultat = mysqli_query($connect, $query);
+
+while ($rekord = mysqli_fetch_assoc($rezultat)){
+    $tabela .= "<tr><td>".$rekord['id']."</td><td>".$rekord['imie']."</td><td>".$rekord['nazwisko']."</td><td>".$rekord['telefon']."</td><td>".$rekord['adres']."</td></tr>";
+}
+
+$tabela .= "</table>";
+
+echo $tabela;
+
+?>
+
+<a href="index.php?modul=klient_formularz" class="dodaj">Dodaj klienta</a>
--- pliki/klient_dane.php
+++ pliki/klient_dane.php
@@ -10,8 +10,9 @@ echo "Telefon: ".$telefon."<br/>";
 echo "Adres: ".$adres."<br/>";
 
 $query = "INSERT INTO `klient`(`imie`, `nazwisko`, `telefon`, `adres`) VALUES ('".$imie."','".$nazwisko."','".$telefon."','".$adres."')";
+$rezultat = mysqli_query($connect, $query);
 
-if(mysqli_query($connect, $query)){
+if($rezultat){
     echo "Dodano klienta<br/>";
 }
 ?>
