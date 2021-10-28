a9caa68b4f5dcc4c3c7f8fb4f16e5de81e39b4d5
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2021-10-28 21:59:33 +0200

    Edycja danych w bazie
    Edycja danych w bazie działa

--- pliki/klient_edytuj.php
+++ pliki/klient_edytuj.php
@@ -1,4 +1,26 @@
 <?php
+if(isset($_GET['aktualizuj'])){
+    $imie = $_POST['imie'];
+    $nazwisko = $_POST['nazwisko'];
+    $telefon = $_POST['telefon'];
+    $adres = $_POST['adres'];
+    $aktualizuj = $_GET['aktualizuj'];
+
+    echo "Zmienione dane: <br/>";
+    echo "Imię: ".$imie."<br/>";
+    echo "Nazwisko: ".$nazwisko."<br/>";
+    echo "Telefon: ".$telefon."<br/>";
+    echo "Adres: ".$adres."<br/>";
+
+    $query = "UPDATE `klient` SET `id`='".$aktualizuj."', `imie`='".$imie."',`nazwisko`='".$nazwisko."',`telefon`='".$telefon."',`adres`='".$adres."' WHERE `id`=".$aktualizuj; 
+    
+    if(mysqli_query($connect, $query)){
+        echo "Baza została zaaktualizowana!";
+    } else {
+        echo "Błąd";
+    }
+}
+
 if(isset($_GET['id'])){
     $id = $_GET['id'];
     $query = "SELECT * FROM `klient` WHERE `id`=$id";
@@ -6,7 +28,7 @@ if(isset($_GET['id'])){
     $rekord = mysqli_fetch_assoc($rezultat);
 ?>
 
-<form action="index.php?modul=klient_edytuj" method="post">
+<form action="index.php?modul=klient_edytuj&amp;aktualizuj=<?php echo $rekord['id'];?>" method="post">
     <fieldset>
         <legend>Edytuj dane klienta:</legend>
         <label for="imie">Imię:</label>
