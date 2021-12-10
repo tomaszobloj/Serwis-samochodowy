5db9ee22a84c1e5d67211f7651e5fc6877ee8ec0
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2021-12-10 16:29:33 +0100

    Edycja id klienta w samochodzie

--- pliki/samochod_edytuj.php
+++ pliki/samochod_edytuj.php
@@ -1,18 +1,20 @@
 <?php
 if(isset($_GET['aktualizuj'])){
+    $id_klient = $_POST['id_klient'];
     $marka = $_POST['marka'];
     $model = $_POST['model'];
     $rocznik = $_POST['rocznik'];
     $przebieg = $_POST['przebieg'];
     $aktualizuj = $_GET['aktualizuj'];
 
+    echo "Klient :".$id_klient."<br/>";
     echo "Zmienione dane samochodu: <br/>";
     echo "Marka samochodu: ".$marka."<br/>";
     echo "Model: ".$model."<br/>";
     echo "Rocznik: ".$rocznik."<br/>";
     echo "Przebieg: ".$przebieg."<br/>";
 
-    $query = "UPDATE `samochod` SET `id`='".$aktualizuj."', `marka`='".$marka."', `model`='".$model."', `rocznik`='".$rocznik."', `przebieg`='".$przebieg."' WHERE `id`=".$aktualizuj;
+    $query = "UPDATE `samochod` SET `id`='".$aktualizuj."', `id_kient`='".$id_klient."', `marka`='".$marka."', `model`='".$model."', `rocznik`='".$rocznik."', `przebieg`='".$przebieg."' WHERE `id`=".$aktualizuj;
 
     if(mysqli_query($connect, $query)){
         echo "Baza została zaaktualizowana!";
@@ -30,16 +32,31 @@ if(isset($_GET['id'])){
 <form action="index.php?modul=samochod_edytuj&amp;aktualizuj=<?php echo $rekord['id'];?>" method="POST">
   <fieldset>
     <legend>Dane samochodu:</legend>
-    <label for="marka">Marka:</label>
-    <input type="text" name="marka" value="<?php echo $rekord['marka'];?>"><br/>
-    <label>Model:</label>
-    <input type="text" name="model" value="<?php echo $rekord['model'];?>"><br/>
-    <label>Rocznik:</label>
-    <input type="number" name="rocznik" value="<?php echo $rekord['rocznik'];?>"><br/>
-    <label>Przebieg:</label>
-    <input type="number" name="przebieg" value="<?php echo $rekord['przebieg'];?>"><br/>
-    <input type="hidden" name="aktualizuj" value="<?php echo $rekord['id'];?>">
-    <input type="submit" value="Wyślij">
+      <label for="klient">Klient:</label><br>
+      <?php
+        function lista($dane, $name){
+          echo "<select name='".$name."'>";
+          while($rekord = mysqli_fetch_assoc($dane)){
+            echo "<option value='".$rekord['id']."'>".$rekord['nazwisko']."</option>";
+          }
+        echo "</select><br>";
+        }
+        
+        $query = "SELECT * FROM `klient`";
+        $rezultat = mysqli_query($connect, $query);
+        
+        lista($rezultat, "id_klient");
+      ?>
+      <label for="marka">Marka:</label>
+      <input type="text" name="marka" value="<?php echo $rekord['marka'];?>"><br/>
+      <label>Model:</label>
+      <input type="text" name="model" value="<?php echo $rekord['model'];?>"><br/>
+      <label>Rocznik:</label>
+      <input type="number" name="rocznik" value="<?php echo $rekord['rocznik'];?>"><br/>
+      <label>Przebieg:</label>
+      <input type="number" name="przebieg" value="<?php echo $rekord['przebieg'];?>"><br/>
+      <input type="hidden" name="aktualizuj" value="<?php echo $rekord['id'];?>">
+      <input type="submit" value="Wyślij">
   </fieldset>
 </form>
 
