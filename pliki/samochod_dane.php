2f5e9db66f8eace79fec06fab6d4ef4daac82ab9
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2021-12-10 16:25:24 +0100

    Wybór klienta w dodawaniu samochodu

--- pliki/samochod_dane.php
+++ pliki/samochod_dane.php
@@ -1,15 +1,17 @@
 <?php
+$id_klient = $_POST['id_klient'];
 $marka = $_POST['marka'];
 $model = $_POST['model'];
 $rocznik = $_POST['rocznik'];
 $przebieg = $_POST['przebieg'];
 
+echo "Klient :".$id_klient."<br/>";
 echo "Marka samochodu: ".$marka."<br/>";
 echo "Model: ".$model."<br/>";
 echo "Rocznik: ".$rocznik."<br/>";
 echo "Przebieg: ".$przebieg."<br/>";
 
-$query = "INSERT INTO `samochod`(`marka`, `model`, `rocznik`, `przebieg`) VALUES ('".$marka."','".$model."','".$rocznik."','".$przebieg."')";
+$query = "INSERT INTO `samochod`(`id_kient`, `marka`, `model`, `rocznik`, `przebieg`) VALUES ('".$id_klient."', '".$marka."','".$model."','".$rocznik."','".$przebieg."')";
 
 if(mysqli_query($connect, $query)){
     echo "Dodano samochód<br/>";
--- pliki/samochod_formularz.php
+++ pliki/samochod_formularz.php
@@ -1,14 +1,29 @@
 <form action="index.php?modul=samochod_dane" method="POST">
   <fieldset>
     <legend>Dane samochodu:</legend>
-    <label for="marka">Marka:</label>
-    <input type="text" name="marka"><br/>
-    <label>Model:</label>
-    <input type="text" name="model"><br/>
-    <label>Rocznik:</label>
-    <input type="number" name="rocznik"><br/>
-    <label>Przebieg:</label>
-    <input type="number" name="przebieg"><br/>
-    <input type="submit" value="Wyślij">
+      <label for="klient">Klient:</label><br>
+      <?php
+        function lista($dane, $name){
+          echo "<select name='".$name."'>";
+          while($rekord = mysqli_fetch_assoc($dane)){
+            echo "<option value='".$rekord['id']."'>".$rekord['nazwisko']."</option>";
+          }
+        echo "</select><br>";
+        }
+        
+        $query = "SELECT * FROM `klient`";
+        $rezultat = mysqli_query($connect, $query);
+        
+        lista($rezultat, "id_klient");
+      ?>
+      <label for="marka">Marka:</label>
+      <input type="text" name="marka"><br>
+      <label>Model:</label>
+      <input type="text" name="model"><br>
+      <label>Rocznik:</label>
+      <input type="number" name="rocznik"><br>
+      <label>Przebieg:</label>
+      <input type="number" name="przebieg"><br>
+      <input type="submit" value="Wyślij">
   </fieldset>
 </form>
