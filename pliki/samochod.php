e278e8094224155885188b997f195ff92914a389
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2022-01-09 17:32:59 +0100

    Wyszukiwanie danych samochodu

--- pliki/samochod.php
+++ pliki/samochod.php
@@ -7,8 +7,34 @@ if(isset($_GET['usun'])){
     }
 }
 
-$tabela = "<table><tr><th>Id</th><th>Marka</th><th>Model</th><th>Rocznik</th><th>Przebieg</th><th>Edytuj</th><th>Usuń</th></tr>";
-$query = "SELECT * FROM `samochod`";
+$wyszukajMarka = '';
+$wyszukajModel = '';
+$wyszukajRocznik = '';
+$wyszukajPrzebieg = '';
+$tabela = "<table><tr><th>Id</th><th>Marka</th><th>Model</th><th>Rocznik</th><th>Przebieg</th></tr>";
+$tabela .= '<tr><td>Wyszukaj</td><td><form action="" method="post"><input type="text" name="marka" value="'.$wyszukajMarka.'"></td><td><input type="text" name="model" value="'.$wyszukajModel.'"></td><td><input type="text" name="rocznik" value="'.$wyszukajRocznik.'"></td><td><input type="text" name="przebieg" value="'.$wyszukajPrzebieg.'"></td><td colspan="2"><input type="submit" value="Szukaj"></td></tr>';
+$query = "SELECT * FROM `samochod` WHERE 1=1";
+
+if(isset($_POST['marka'])){
+	$query .= ' AND marka LIKE "%'.$_POST['marka'].'%"';
+	$wyszukajMarka = $_POST['marka'];
+}
+
+if(isset($_POST['model'])){
+	$query .= ' AND model LIKE "%'.$_POST['model'].'%"';
+	$wyszukajModel = $_POST['model'];
+}
+
+if(isset($_POST['rocznik'])){
+	$query .= ' AND rocznik LIKE "%'.$_POST['rocznik'].'%"';
+	$wyszukajRocznik = $_POST['rocznik'];
+}
+
+if(isset($_POST['przebieg'])){
+	$query .= ' AND przebieg LIKE "%'.$_POST['przebieg'].'%"';
+	$wyszukajPrzebieg = $_POST['przebieg'];
+}
+
 $rezultat = mysqli_query($connect, $query);
 
 while ($rekord = mysqli_fetch_assoc($rezultat)){
