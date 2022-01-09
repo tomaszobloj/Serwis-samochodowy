758b8f214ebc7157a1d080a3b1b1b21f0aa2c0b0
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2022-01-09 17:46:32 +0100

    Wyszukiwanie danych części

--- pliki/czesci.php
+++ pliki/czesci.php
@@ -7,8 +7,28 @@ if(isset($_GET['usun'])){
     }
 }
 
-$tabela = "<table><tr><th>Id</th><th>Nazwa</th><th>Cena</th><th>Stan</th><th>Edytuj</th><th>Usuń</th></tr>";
-$query = "SELECT * FROM `czesc`";
+$wyszukajNazwa = '';
+$wyszukajCena = '';
+$wyszukajStan = '';
+$tabela = "<table><tr><th>Id</th><th>Nazwa</th><th>Cena</th><th>Stan</th></tr>";
+$tabela .= '<tr><td>Wyszukaj</td><td><form action="" method="post"><input type="text" name="nazwa" value="'.$wyszukajNazwa.'"></td><td><input type="text" name="cena" value="'.$wyszukajCena.'"></td><td><input type="text" name="stan_magazynowy" value="'.$wyszukajStan.'"></td><td colspan="2"><input type="submit" value="Szukaj"></td></tr>';
+$query = "SELECT * FROM `czesc` WHERE 1=1";
+
+if(isset($_POST['nazwa'])){
+	$query .= ' AND nazwa LIKE "%'.$_POST['nazwa'].'%"';
+	$wyszukajNazwa = $_POST['nazwa'];
+}
+
+if(isset($_POST['cena'])){
+	$query .= ' AND cena LIKE "%'.$_POST['cena'].'%"';
+	$wyszukajCena = $_POST['cena'];
+}
+
+if(isset($_POST['stan_magazynowy'])){
+	$query .= ' AND stan_magazynowy LIKE "%'.$_POST['stan_magazynowy'].'%"';
+	$wyszukajStan = $_POST['stan_magazynowy'];
+}
+
 $rezultat = mysqli_query($connect, $query);
 
 while ($rekord = mysqli_fetch_assoc($rezultat)){
